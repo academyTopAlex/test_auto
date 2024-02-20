9a8858545298335a393f8165551f47a0f30d7f8e
Your Name <you@example.com>
2024-02-20 20:52:57 +0300

    init

---
+++ main.py
@@ -0,0 +1,8 @@
+
+
+def division(x, y):
+    return x / y
+
+
+# tests - название пакета
+# test_* - название файла и функций
---
+++ tests/test_division.py
@@ -0,0 +1,18 @@
+from main import division
+import pytest
+
+
+@pytest.mark.parametrize("a, b, result", [(10, 5, 2),
+                                          (100, 2, 50),
+                                          (50, 2, 25),
+                                          (60, 30, 2),
+                                          (60, 30, 2)])
+def test_division_good(a, b, result):
+    assert division(a, b) == result
+
+
+@pytest.mark.parametrize("x, error", [(0, ZeroDivisionError), ("dd", TypeError)])
+def test_division_error(x, error):
+    with pytest.raises(error):
+        division(5, x)
+
